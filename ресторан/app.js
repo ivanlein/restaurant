f850f120e94215bc70f689597a08278858ecf6db
ivanlein <vanyaexplay@gmail.com>
2024-06-18 03:29:35 +0300

    restaurant
    restaurant

---
+++ ресторан/app.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () => {
+    if (menu.classList.contains('disp') == true) {
+        menu.classList.remove('disp');
+    } else {
+        menu.classList.add('disp');
+    }
+});
